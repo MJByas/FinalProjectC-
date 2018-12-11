d3383c988556cebfcf810fd339cc9b73e5060108
MJByas <45529233+MJByas@users.noreply.github.com>
2018-12-11 14:58:31 -0800

    Update Source.cpp

--- Final Project/Final Project/Source.cpp
+++ Final Project/Final Project/Source.cpp
@@ -40,19 +40,66 @@ int main() {
 
 	if (charClass == 1) {
 		cout << "Oh I should've guessed from the armor!" << endl;
+		Warrior.SnkAtk = "# Your attack fails because you're loud and you are hit";
+		Warrior.HOAtk = "# You charge your enemy with a mighty blow and defeat him";
 		Warrior.hp = 10;
-		Warrior.abi = "Gigaslash";
 	}
 	else if (charClass == 2) {
 		cout << "I love seeing mages cast fireballs!" << endl;
-		Mage.hp = 5;
-		Mage.abi = "Fireball";
+		Mage.SnkAtk = "# You charge up your fireball and throw it directly at the enemy";
+		Mage.HOAtk = "# You are attacked mid spell charge and injured";
+		Mage.hp = 6;
 	}
 	else {
 		cout << "No stealing from me now!" << endl;
-		Thief.hp = 7;
-		Thief.abi = "Smoke bomb";
+		Thief.SnkAtk = "# You sneak up on the enemy and cut them down from behind with your dagger";
+		Thief.HOAtk = "# You charge the enemy but you don't pierce his armor and he throws you back";
+		Thief.hp = 8;
 	}
+	cout << "Anyways let's move to the first location. We need to take this camp back from the orcs." << endl;
+	cout << "Wanna sneak up and attack or head on attack?" << endl
+		<< "1. Head on attack" << endl
+		<< "2. Sneak attack" << endl;
+	cin >> choices[0];
+	switch (choices[0]) {
+	case 1:
+		if (charClass == 1)
+			cout << Warrior.HOAtk << endl
+			<< "You beat him in just one hit!" << endl << endl;
+		else if (charClass == 2) {
+			cout << Mage.HOAtk << endl
+				<< "# Ulfang takes out the orc attacking you from behind" << endl
+				<< "You need time to charge up your spells, mage." << endl << endl;
+			Mage.hp - 2;
+		}
+		else {
+			cout << Thief.HOAtk << endl
+				<< "# You get back up after hitting your head and stab the orc in" << endl
+				<< "# a weak spot with your dagger, thus killing him" << endl
+				<< "Nice bounce back thief." << endl << endl;
+			Thief.hp - 2;
+		}
+	case 2: 
+		if (charClass == 1) {
+			cout << Warrior.SnkAtk << endl
+				<< "# The orc charges you but you raise your sword just in time to kill him" << endl
+				<< "That was a close one, warrior." << endl << endl;
+			Warrior.hp - 3;
+		}
+		else if (charClass == 2)
+			cout << Mage.SnkAtk << endl
+			<< "# The fireball is so strong it kills both orcs" << endl
+			<< "God I love seeing fireballs exlpode!" << endl << endl;
+		else
+			cout << Thief.SnkAtk << endl
+			<< "# Ulfang takes out his orc in a similar fashion" << endl
+			<< "You are one pretty sneaky thief." << endl << endl;
+	}
+	cout << "We've taken the camp back" << endl
+		<< "We need to rest here for the night" << endl
+		<< "# You two rest for the night to gein some energy" << endl
+		<< "# You awake to see a wandering gambler down the road" << endl << endl;
+	Sleep(1000);
 }
 
 void luck () {														// Random number generator with a seed of 0
